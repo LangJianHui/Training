56c030f50bd975ce141e1932afc04847e0c475dc
LangJianHui <93663126@qq.com>
2019-09-21 14:52:54 +0800

    去掉了部分注解

--- src/com/aaa/mrlang/entity/SysDictType.java
+++ src/com/aaa/mrlang/entity/SysDictType.java
@@ -1,7 +1,6 @@
 package com.aaa.mrlang.entity;
 
 
-import com.aaa.mrlang.common.annotation.Excel;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -17,19 +16,15 @@ public class SysDictType extends BaseEntity
     private static final long serialVersionUID = 1L;
 
     /** 字典主键 */
-    @Excel(name = "字典主键", cellType = Excel.ColumnType.NUMERIC)
     private Long dictId;
 
     /** 字典名称 */
-    @Excel(name = "字典名称")
     private String dictName;
 
     /** 字典类型 */
-    @Excel(name = "字典类型 ")
     private String dictType;
 
     /** 状态（0正常 1停用） */
-    @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
     private String status;
 
     public Long getDictId()
